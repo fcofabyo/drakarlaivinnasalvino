62109a75a698c124f39701a2a35404c8b73f0de9
sabrina moura <topvedacoesdistribuidora@gmail.com>
2026-07-10 11:01:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// ==========================
+// ANO AUTOMÁTICO NO FOOTER
+// ==========================
+
+const footer = document.querySelector("footer");
+
+footer.innerHTML =
+`© ${new Date().getFullYear()} Desenvolvido por <strong>BenVRA®</strong>`;
+
+
+// ==========================
+// EFEITO RIPPLE NOS BOTÕES
+// ==========================
+
+const botoes = document.querySelectorAll(".btn");
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", function(e){
+
+        const ripple = document.createElement("span");
+
+        const rect = this.getBoundingClientRect();
+
+        ripple.style.left = (e.clientX - rect.left) + "px";
+        ripple.style.top = (e.clientY - rect.top) + "px";
+
+        ripple.classList.add("ripple");
+
+        this.appendChild(ripple);
+
+        setTimeout(()=>{
+
+            ripple.remove();
+
+        },600);
+
+    });
+
+});
